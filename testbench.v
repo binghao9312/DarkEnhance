3026275e4c479a3e6ba35669daab63c5e2356fec
binghao9312 <a0938676069@gmail.com>
2025-07-28 15:25:58 +0800

    fixed pixel_input before rst

--- testbench.v
+++ testbench.v
@@ -4,27 +4,30 @@ module testbench;
 
 reg clk;
 reg rst;
-wire RW_bar;
 
+wire RW_bar;
 wire [7:0] pixel_R, pixel_G, pixel_B;
-reg [23:0] img_mem [0:262143];   // 512x512
+
+reg [23:0] img_mem [0:262143]; // 512x512
 reg [17:0] img_idx;
+
 reg [7:0] pixel_in_R, pixel_in_G, pixel_in_B;
+
 wire done;
 
-// Clock generator: 10ns period
+// ===== Clock generator: 10ns period
 initial begin
     clk = 0;
     forever #5 clk = ~clk;
 end
 
-// Load image memory
+// ===== Image memory pre-load
 initial begin
     $readmemh("output.hex", img_mem);
     $display("img_mem[0]=%h, img_mem[1]=%h", img_mem[0], img_mem[1]);
 end
 
-// Reset and simulation flow
+// =====_RESET、img_idx、像素初始化與模擬流程控制
 initial begin
     img_idx = 18'd0;
     pixel_in_R = 8'd0;
@@ -32,26 +35,30 @@ initial begin
     pixel_in_B = 8'd0;
     rst = 1;
     #20;
-    rst = 0;
-    pixel_in_R = img_mem[img_idx][23:16];
-    pixel_in_G = img_mem[img_idx][15:8];
-    pixel_in_B = img_mem[img_idx][7:0];
-    //$display("Feed #%0d  R: %d, G: %d, B: %d", img_idx, pixel_in_R, pixel_in_G, pixel_in_B);
-    img_idx = img_idx + 1;
-    wait(done);
+    rst = 0;          // 拉低 reset 之後才會開始出資料
+    wait(done);       // 等待 DUT done
     $display("Simulation finished.");
-    #100; // Finish after short delay
+    #100;
     $finish;
 end
 
+// =====clock驅動像素傳送
+always @(posedge clk or rst) begin
+    if (rst) begin
+        img_idx      <= 0;
+        pixel_in_R   <= 8'd0;
+        pixel_in_G   <= 8'd0;
+        pixel_in_B   <= 8'd0;
+    end 
+    else if (!done) begin
+        pixel_in_R   <= img_mem[img_idx][23:16];
+        pixel_in_G   <= img_mem[img_idx][15:8];
+        pixel_in_B   <= img_mem[img_idx][7:0];
+        img_idx      <= img_idx + 1;
+    end
+end
 
-// Output monitoring (once done is high)
-//always @(posedge clk) begin
-    //if(done) begin
-        //$display("pixel_R: %h, pixel_G: %h, pixel_B: %h", pixel_R, pixel_G, pixel_B);
-    //end
-//end
-
+// ===== Module Instantiation
 top DUT (
     .clk(clk),
     .rst(rst),
