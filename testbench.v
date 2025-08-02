47f5af7e0c5d8a6978e7b9f75097033c8d0d841f
binghao9312 <a0938676069@gmail.com>
2025-08-02 21:28:56 +0800

    pixel_in port changed to combination

--- testbench.v
+++ testbench.v
@@ -11,7 +11,7 @@ wire [7:0] pixel_R, pixel_G, pixel_B;
 reg [23:0] img_mem [0:262143]; // 512x512
 reg [17:0] img_idx;
 
-reg [7:0] pixel_in_R, pixel_in_G, pixel_in_B;
+wire [7:0] pixel_in_R, pixel_in_G, pixel_in_B;
 
 wire done;
 
@@ -32,9 +32,6 @@ end
 // =====_RESET、img_idx、像素初始化與模擬流程控制
 initial begin
     img_idx = 18'd0;
-    pixel_in_R = 8'd0;
-    pixel_in_G = 8'd0;
-    pixel_in_B = 8'd0;
     rst = 1;
     #20;
     rst = 0;          // 拉低 reset 之後才會開始出資料
@@ -47,14 +44,9 @@ end
 always @(negedge clk or rst) begin
     if (rst) begin
         img_idx      <= 0;
-        pixel_in_R   <= 8'd0;
-        pixel_in_G   <= 8'd0;
-        pixel_in_B   <= 8'd0;
     end 
     else if (!RW_bar) begin
-        pixel_in_R   <= img_mem[img_idx][23:16];
-        pixel_in_G   <= img_mem[img_idx][15:8];
-        pixel_in_B   <= img_mem[img_idx][7:0];
+        
         if(!input_pause)begin
             img_idx <= img_idx + 1;
         end
@@ -64,8 +56,9 @@ always @(negedge clk or rst) begin
     end
 end
 
-
-
+assign pixel_in_R  = (!input_pause)? img_mem[img_idx][23:16] : 8'dz;
+assign pixel_in_G  = (!input_pause)? img_mem[img_idx][15: 8] : 8'dz;
+assign pixel_in_B  = (!input_pause)? img_mem[img_idx][ 7: 0] : 8'dz;
 // ===== Module Instantiation
 top DUT (
     .clk(clk),
